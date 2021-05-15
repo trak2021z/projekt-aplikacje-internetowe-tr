f94e7b73381b613cea476477dc12c80ea9b27187
Aleksander Kobylski <148832@stud.prz.edu.pl>
2021-05-15 13:24:42 +0200

    Initial commit

---
+++ src/test/java/com/example/projektaplikacjeinternetowe/ProjektAplikacjeInternetoweApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projektaplikacjeinternetowe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektAplikacjeInternetoweApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
